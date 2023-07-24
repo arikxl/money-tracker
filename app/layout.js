0f42e704402fe05ee491e5b58abd7e571ffa7740
Arik Alexandrov <arikxl@gmail.com>
2023-07-25 00:43:34 +0300

    deploy3

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-'use client'
 
 import { Inter } from 'next/font/google'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import './globals.css'
